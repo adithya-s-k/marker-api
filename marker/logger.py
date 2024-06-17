fbf73858099997b816d0309924f4c35d233a8c9c
Adithya S K <adithyaskolavi@gmail.com>
2024-06-18 00:47:49 +0530

    logger updated - Adithya S K

--- marker/logger.py
+++ marker/logger.py
@@ -1,40 +1,12 @@
 import logging
 import logging.config
-from marker.settings import Settings
 
+import logging
+import warnings
 def configure_logging():
-    settings = Settings()
-    
-    logging_config = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'standard': {
-                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            },
-        },
-        'handlers': {
-            'default': {
-                'level': 'DEBUG',
-                'class': 'logging.StreamHandler',
-                'formatter': 'standard',
-            },
-            'file': {
-                'level': 'DEBUG',
-                'class': 'logging.handlers.RotatingFileHandler',
-                'filename': settings.LOG_FILE,
-                'maxBytes': 10485760,
-                'backupCount': 3,
-                'formatter': 'standard',
-            },
-        },
-        'loggers': {
-            '': {
-                'handlers': ['default', 'file'],
-                'level': 'DEBUG',
-                'propagate': True
-            },
-        }
-    }
-
-    logging.config.dictConfig(logging_config)
+    logging.basicConfig(level=logging.WARNING)
+    logging.getLogger('pdfminer').setLevel(logging.ERROR)
+    logging.getLogger('PIL').setLevel(logging.ERROR)
+    logging.getLogger('fitz').setLevel(logging.ERROR)
+    logging.getLogger('ocrmypdf').setLevel(logging.ERROR)
+    warnings.simplefilter(action='ignore', category=FutureWarning)
